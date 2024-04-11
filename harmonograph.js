0895374636111d2a625e8a13fe4c5b1208555b00
The Jabberwock <zazyzaya@gmail.com>
2024-04-11 13:54:31 -0400

    prototype working

--- harmonograph.js
+++ harmonograph.js
@@ -1,15 +1,28 @@
-const W = window.innerWidth;
-const H = window.innerHeight; 
+let W,H; 
+let p1,p2,p3;
+var t_frame=null;
+let last_pen_x,last_pen_y
+let pen_viz = true; 
 
-const p1 = new Pendulum(W/2,H/2);
+window.onload = function() {
+  W = window.innerWidth;
+  H = window.innerHeight; 
+  
+  p1 = new Pendulum(W/2,H/4,generateColor());
+  p2 = new Pendulum(W/4,H/2,generateColor());
+  p3 = new Pendulum(W/2,H/2,generateColor());
+  
+  [last_pen_x,last_pen_y] = combine_pendula();
+  init();
+};
 
-// WHY JAVASCRIPT?!?!?
-function mod(x,div) {
-  var rem = x % div; 
-  if (rem < 0) {
-    return div+rem; 
+function generateColor() {
+  let hexSet = "0123456789ABCDEF";
+  let finalHexString = "#";
+  for (let i = 0; i < 6; i++) {
+    finalHexString += hexSet[Math.ceil(Math.random() * 15)];
   }
-  return rem;
+  return finalHexString;
 }
 
 function set_size(canvas_id) {
@@ -20,18 +33,64 @@ function set_size(canvas_id) {
 function init() {
   // Cant do this in css for some reason
   set_size("canvas");
-  window.requestAnimationFrame(trace);
+  set_size("trace"); 
+  window.requestAnimationFrame(animate);
 }
 
-var t_frame=null;
-function trace() {
-  const ctx = document.getElementById("canvas").getContext("2d");
+function reset() {
+  const ca_ctx = document.getElementById("canvas").getContext("2d");
+  const tr_ctx = document.getElementById("trace").getContext("2d");
+  ca_ctx.clearRect(0, 0, W,H); 
+  tr_ctx.clearRect(0, 0, W,H); 
   
-  ctx.clearRect(0, 0, W,H); 
-  p1.pixel_trail(ctx);
+  p1 = new Pendulum(W/2,H/4,generateColor());
+  p2 = new Pendulum(W/4,H/2,generateColor());
+  p3 = new Pendulum(W/2,H/2,generateColor());
 
-  t_frame = window.requestAnimationFrame(trace);
-
+  [last_pen_x,last_pen_y] = combine_pendula();
 }
 
-init();
+function combine_pendula() {
+  // Translate s.t. center of screen is 0,0
+  // Assumes input vectors are relative to pendula tip
+  let [pen_x,pen_y] = p3.coords(tip=true);
+  pen_x += W/2; pen_y += H/2;
+
+  let [x1,y1] = p1.coords(tip=true); 
+  let [x2,y2] = p2.coords(tip=true);
+
+  // This needs to be adjusted if pendula aren't 
+  // Orthoganal with pen at intersection
+  return [pen_x +x1+x2, pen_y +y1+y2];
+}
+
+function animate() {
+  p1.update(); 
+  p2.update();
+  p3.update();
+
+  const ca_ctx = document.getElementById("canvas").getContext("2d");
+  const tr_ctx = document.getElementById("trace").getContext("2d");
+
+  // Draw the weights
+  ca_ctx.clearRect(0, 0, W,H); 
+  if (pen_viz) {
+    p1.draw(ca_ctx);
+    p2.draw(ca_ctx);
+    p3.draw(ca_ctx);
+  }
+
+  // Trace the paths 
+  let [pen_x, pen_y] = combine_pendula()
+  tr_ctx.beginPath();
+  tr_ctx.strokeStyle = document.getElementById("pen").value; 
+
+  tr_ctx.moveTo(last_pen_x, last_pen_y); 
+  tr_ctx.lineTo(pen_x, pen_y); 
+  tr_ctx.stroke(); 
+
+  last_pen_x = pen_x; 
+  last_pen_y = pen_y;
+
+  t_frame = window.requestAnimationFrame(animate);
+}
--- pendulum.js
+++ pendulum.js
@@ -1,54 +1,57 @@
-function generateColor() {
-    let hexSet = "0123456789ABCDEF";
-    let finalHexString = "#";
-    for (let i = 0; i < 6; i++) {
-      finalHexString += hexSet[Math.ceil(Math.random() * 15)];
-    }
-    return finalHexString;
-  }
-
 class Pendulum {
-    constructor(x,y, l=10, lambda=0.99) {
+    constructor(x,y,c, l=5000,tip_l=1, lambda=0.9999) {
         // X,y are coords when pendulum viewed from above 
         this.x = x; 
         this.y = y;
-        this.l = l; 
-        this.lambda = lambda; // Friction
+        this.color = c; 
 
-        this.color = generateColor();
+        this.l = l; 
+        this.tip_l = tip_l; 
+        this.lambda = lambda; // Friction
         this.dt = 0.01;
 
         // Angle when observed from the side wrt x
-        this.theta = Math.cos(x / l)
-        this.theta_v = 0; 
+        this.theta = Math.random()*0.1 -0.05;
+        this.theta_v = Math.random()/25 - 0.05; 
 
         // Angle off of x when obseved from above
         this.phi = Math.tanh(y/x); 
-        this.phi_v = 0; 
+        this.phi_v = Math.random() * 10 - 5; 
+
+        this.last_x = NaN; 
+        this.last_y = NaN; 
     }
 
-    coords() {
-        let x = this.x + (this.l * Math.sinh(this.theta)); 
-        let y = this.y + (x * Math.tan(this.phi)); 
+    coords(tip=false) {
+        let lst = this.l * Math.sin(this.theta); 
+        let dx = lst*Math.cos(this.phi); 
+        let dy = lst*Math.sin(this.phi)
 
-        return [x,y];
+        if (tip) {
+            // Coords are flipped and have 
+            // [x,y]*tip_l magnitude 
+            return [-dx*this.tip_l,-dy*this.tip_l];
+        }
+
+        return [this.x+dx, this.y+dy];
     }
 
     update() {
+        let [x,y] = this.coords();
+        this.last_x = x; 
+        this.last_y = y; 
+
         // Assume g/l = 1 
         let a_theta = -Math.sin(this.theta); 
         this.theta_v += a_theta; 
         this.theta_v *= this.lambda;
         this.theta += this.theta_v*this.dt; 
 
-        let a_phi = -Math.sin(this.phi);
-        this.phi_v += a_phi; 
-        this.phi_v *= this.lambda; 
+        //this.phi_v *= this.lambda; 
         this.phi += this.phi_v*this.dt;
-
     }
 
-    pixel_trail(ctx) {
+    draw(ctx) {
         ctx.beginPath();
         ctx.fillStyle = this.color; 
 
@@ -57,7 +60,17 @@ class Pendulum {
         let [x,y] = this.coords(); 
         ctx.arc(x, y, 10, size, 2 * Math.PI, false);
         ctx.fill();
+    }
 
-        this.update(); 
+    trace(ctx) {
+        ctx.beginPath();
+        ctx.strokeStyle = this.color; 
+
+        let size = 0.1;
+        let [x,y] = this.coords(); 
+
+        ctx.moveTo(this.last_x, this.last_y); 
+        ctx.lineTo(x, y); 
+        ctx.stroke(); 
     }
 }
